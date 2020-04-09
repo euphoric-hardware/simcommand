71fd2da95b9d65175f284bc7da3a7ce085502151
Thonner <anthon.riber@yahoo.com>
2020-04-09 18:04:44 +0200

    More neuron eval test - not done

--- src/test/scala/NeuronsTester.scala
+++ src/test/scala/NeuronsTester.scala
@@ -17,6 +17,44 @@ class NeuronEvaluatorTest(dut: NeuronEvaluator) extends PeekPokeTester(dut) {
 
   expect(dut.io.spikeIndi, 0)
   expect(dut.io.refracIndi, 1)
+
+  // load 10 weight and check membrane potential
+  poke(dut.io.cntrSels.potSel, 1)
+  var totalweight = 0
+  for(i <- 0 to 10){
+    poke(dut.io.dataIn, i)
+    step(1)
+    totalweight += i
+  }
+  poke(dut.io.cntrSels.potSel, 2)
+  poke(dut.io.dataIn, 0)
+  poke(dut.io.cntrSels.writeDataSel, 1)
+  expect(dut.io.dataOut, 12 + totalweight)
+
+  //check threshold/spike indication 
+
+  //not exceeded thres
+  poke(dut.io.dataIn, 12 + totalweight + 10)
+  poke(dut.io.cntrSels.spikeSel, 0)
+  step(1)
+  poke(dut.io.cntrSels.spikeSel, 1)
+  poke(dut.io.dataIn, 0)
+  poke(dut.io.cntrSels.writeDataSel, 1)
+  expect(dut.io.dataOut, 12 + totalweight)
+  expect(dut.io.spikeIndi, 0)
+  step(1)
+
+  //exceeded thres
+  poke(dut.io.dataIn, 12 + totalweight - 10)
+  poke(dut.io.cntrSels.spikeSel, 0)
+  step(1)
+  poke(dut.io.cntrSels.spikeSel, 1)
+  poke(dut.io.dataIn, 0)
+  poke(dut.io.cntrSels.writeDataSel, 1)
+  expect(dut.io.dataOut, 0)
+  expect(dut.io.spikeIndi, 1)
+
+
   
 }
 
