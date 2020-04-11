cb5b306721a2b72c2295aaad4d55438c2a9664d2
Thonner <anthon.riber@yahoo.com>
2020-04-11 13:48:10 +0200

    More constants

--- src/main/scala/Constants.scala
+++ src/main/scala/Constants.scala
@@ -2,7 +2,11 @@ import chisel3._
 import util._
 
 object Constants{
+    val CORES        = 4
     val NEUDATAWIDTH = 16
     val AXONNR       = 1024
     val AXONIDWIDTH  = log2Up(AXONNR)
+    val TMPNEURONS   = 32
+    val N            = log2Up(TMPNEURONS)
+    val EVALUNITS    = 8
 }
