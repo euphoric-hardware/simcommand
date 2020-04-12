63d8f98b7b4fecec75249554398a6b0f3bf340a2
Thonner <anthon.riber@yahoo.com>
2020-04-12 13:52:10 +0200

    Implemented not tested spike transmission system

--- src/main/scala/SpikeTransmission.scala
+++ src/main/scala/SpikeTransmission.scala
@@ -2,7 +2,7 @@ import chisel3._
 import util._
 import Constants._
 
-class TransmissionSystem extends Module {
+class TransmissionSystem(coreID: Int) extends Module {
   val io = IO(new Bundle {
     //For Communication fabric interface
     val ack = Input(Bool())
@@ -17,7 +17,43 @@ class TransmissionSystem extends Module {
 
   val spikeRegs   = RegInit(VecInit(Seq.fill(EVALUNITS)(false.B)))
   val neuronIdLSB = RegInit(VecInit(Seq.fill(EVALUNITS)(0.U(N.W))))
+  val maskRegs    = RegInit(VecInit(Seq.fill(EVALUNITS)(false.B)))
 
+  val spikeEncoder = Module(new PriorityMaskRstEncoder)
+  val encoderReqs  = Wire(Vec(EVALUNITS, Bool()))
+  val rstAckSel    = Wire(Vec(EVALUNITS, Bool()))
+  val spikeUpdate  = Wire(Vec(EVALUNITS, Bool()))
+
+  spikeEncoder.io.reqs := encoderReqs
+  
+  io.data := 0.U //Default assignment for compiler - never the case
+  io.valid := spikeEncoder.io.valid
+
+
+  for(i <- 0 to EVALUNITS-1){
+    when(io.ack){
+      maskRegs(i) := spikeEncoder.io.mask(i)
+    }
+
+    encoderReqs(i) := maskRegs(i) && spikeRegs(i)
+
+    rstAckSel(i) := ~(spikeEncoder.io.rst(i) && io.ack)
+    spikeUpdate(i) := rstAckSel(i) && spikeRegs(i)
+
+    when(~spikeUpdate(i)){
+      neuronIdLSB(i) := io.ns(i)
+
+      when(io.spikes(i)){
+        spikeRegs(i) := true.B
+      }.otherwise{
+        spikeRegs(i) := false.B
+      }
+    }
+
+    when(i.U === spikeEncoder.io.value){
+      io.data := coreID.U(log2Up(CORES).W) ## spikeEncoder.io.value ## neuronIdLSB(i)
+    }
+  }
 
   //Priority encoder and mask
 
