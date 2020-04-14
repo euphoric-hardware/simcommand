58acb952fc685987b0633041b0b0e0d87efff854
Thonner <anthon.riber@yahoo.com>
2020-04-14 12:55:48 +0200

    Control unit first draft - needs testing

--- src/main/scala/Neurons.scala
+++ src/main/scala/Neurons.scala
@@ -62,7 +62,7 @@ class NeuronEvaluator extends Module {
 
   switch(io.cntrSels.spikeSel) {
     is(0.U){
-    spikeIndiReg := membPotReg > io.dataIn
+      spikeIndiReg := membPotReg > io.dataIn
     }
     is(1.U){
       spikeIndiReg := false.B
@@ -84,9 +84,9 @@ class NeuronEvaluator extends Module {
       when(io.refracIndi){
         io.dataOut := refracCntReg
       }.otherwise{
-      io.dataOut := refracCntReg - 1.U
+        io.dataOut := refracCntReg - 1.U
+      }
     }
-  }
   }
   
   io.refracIndi := refracRegNext === 0.U
@@ -198,13 +198,14 @@ class ControlUnit extends Module{
   }
   )
 
-  val idle :: rRefrac :: rPot :: rWeight1 :: rWeight2 :: rBias :: rDecay :: rThresh :: rRefracSet :: wRefrac :: wPot :: Nil = Enum(11)
+  val idle :: rRefrac :: rPot :: rWeight1 :: rWeight2 :: rBias :: rDecay :: rThresh :: rRefracSet :: wRefrac :: rPotSet :: wPot :: Nil = Enum(11)
   val stateReg = RegInit(idle)
 
   val spikePulse = RegInit(VecInit(Seq.fill(EVALUNITS)(false.B))) // used to deliver spike pulses to transmission
   val tsCycleCnt = RegInit(CYCLESPRSTEP.U)                        //count time step cycles down to 0
   val n          = RegInit(0.U(N.W))                              // time multiplex evaluation counter
-  val a          = RegInit(0.U(AXONIDWIDTH.W))                    //axon system addr counter
+  val aNext      = Wire(UInt(AXONIDWIDTH.W))
+  val a          = RegNext(aNext)                                 //axon system addr counter
   val spikeCnt   = RegInit(0.U(AXONIDWIDTH.W))                    //register that stores sample of axons incoming spike counter
   val inOut      = RegInit(false.B)                               //used to inform spike system of new timestep
 
@@ -212,12 +213,13 @@ class ControlUnit extends Module{
   val addrOffset   = Wire(UInt(EVALMEMADDRWIDTH.W))
   val addrSpecific = Wire(UInt(EVALMEMADDRWIDTH.W))
 
+
   //Default assignments
   for(i <- 0 until EVALUNITS){
     spikePulse(i) := false.B
-    io.cntrSels(i).potSel       := 0.U //TODO reconsider default of control
-    io.cntrSels(i).spikeSel     := 0.U
-    io.cntrSels(i).refracSel    := 0.U
+    io.cntrSels(i).potSel       := 2.U //TODO reconsider default of control - should now be okay
+    io.cntrSels(i).spikeSel     := 2.U
+    io.cntrSels(i).refracSel    := 1.U
     io.cntrSels(i).writeDataSel := 0.U
     
     io.spikes(i)                := spikePulse(i)
@@ -226,9 +228,11 @@ class ControlUnit extends Module{
   io.wr    := false.B
   io.ena   := false.B
   io.inOut := inOut
-  io.aAddr := 0.U //TODO consider if this should be based on reg - probs no
-  io.aEna  := false.B
+  io.aAddr := a
+  io.aEna  := true.B
   io.n     := n
+
+  aNext := a
 
   addrOffset   := 0.U
   addrSpecific := 0.U
@@ -241,7 +245,7 @@ class ControlUnit extends Module{
     tsCycleCnt := CYCLESPRSTEP.U
   }
 
-  
+
 
   switch(stateReg){
     is(idle){
@@ -256,36 +260,168 @@ class ControlUnit extends Module{
     is(rRefrac){
       io.ena       := true.B
       io.wr        := false.B
-      addrOffset   := OSPOTENTIAL.U
+      addrOffset   := OSREFRAC.U
       addrSpecific := n
+
+      for(i <- 0 until EVALUNITS){
+        io.cntrSels(i).spikeSel := 1.U
+      }
+
       stateReg     := rPot
     }
     is(rPot){
-      
+      io.ena       := true.B
+      io.wr        := false.B
+      addrOffset   := OSPOTENTIAL.U
+      addrSpecific := n
+
+      for(i <- 0 until EVALUNITS){
+        io.cntrSels(i).refracSel := 0.U
+      }
+
+      when(spikeCnt === 0.U){
+        stateReg := rBias
+      }.otherwise{
+        stateReg := rWeight1
+      }
     }
     is(rWeight1){
-      
+      io.ena       := true.B
+      io.wr        := false.B
+      addrOffset   := OSWEIGHT.U
+      addrSpecific := n ## io.aData //TODO VERIFY THIS WORKS CONCATINATION
+
+      for(i <- 0 until EVALUNITS){
+        io.cntrSels(i).potSel := 0.U
+      }
+
+      aNext := a + 1.U
+
+      when(spikeCnt === aNext){    //TODO CHECK EDGE CASE
+        stateReg := rBias
+      }.otherwise{
+        stateReg := rWeight2
+      }
     }
     is(rWeight2){
-      
+      io.ena       := true.B
+      io.wr        := false.B
+      addrOffset   := OSWEIGHT.U
+      addrSpecific := n ## io.aData //TODO VERIFY THIS WORKS
+
+      for(i <- 0 until EVALUNITS){
+        when(!io.refracIndi(i)){
+          io.cntrSels(i).potSel := 1.U
+        }
+      }
+      aNext := a + 1.U
+
+      when(spikeCnt === aNext){ //TODO CHECK EDGE CASE
+        stateReg := rBias
+      }.otherwise{
+        stateReg := rWeight2
+      }
     }
     is(rBias){
+      io.ena       := true.B
+      io.wr        := false.B
+      addrOffset   := OSBIAS.U
+      addrSpecific := n
+
+      when(spikeCnt === 0.U){
+        for(i <- 0 until EVALUNITS){
+          io.cntrSels(i).potSel := 0.U
+        }
+      }.otherwise{
+        for(i <- 0 until EVALUNITS){
+          when(!io.refracIndi(i)){
+            io.cntrSels(i).potSel := 1.U
+          }
+        }
+      }
       
+      stateReg := rDecay
     }
     is(rDecay){
+      io.ena       := true.B
+      io.wr        := false.B
+      addrOffset   := OSDECAY.U
+      addrSpecific := n
       
+      for(i <- 0 until EVALUNITS){
+        when(!io.refracIndi(i)){
+          io.cntrSels(i).potSel := 1.U
+        }
+      }
+
+      stateReg := rThresh
+
     }
     is(rThresh){
+      io.ena       := true.B
+      io.wr        := false.B
+      addrOffset   := OSTHRESH.U
+      addrSpecific := n
+      
+      for(i <- 0 until EVALUNITS){ //TODO make sure that we can subtract we probs need to convert all to SINT
+        when(!io.refracIndi(i)){
+          io.cntrSels(i).potSel := 1.U
+        }
+      }
+
+      stateReg := rRefracSet
       
     }
     is(rRefracSet){
-      
+      io.ena       := true.B
+      io.wr        := false.B
+      addrOffset   := OSREFRACSET.U
+      addrSpecific := n
+
+      for(i <- 0 until EVALUNITS){
+        io.cntrSels(i).spikeSel := 0.U
+      }
+
+      stateReg := wRefrac
     }
     is(wRefrac){
-      
+      io.ena       := true.B
+      io.wr        := true.B
+      addrOffset   := OSREFRAC.U
+      addrSpecific := n
+
+      for(i <- 0 until EVALUNITS){
+        io.cntrSels(i).writeDataSel := 2.U
+      }
+
+      stateReg := rPotSet
+
+    }
+    is(rPotSet){
+      io.ena       := true.B
+      io.wr        := false.B
+      addrOffset   := OSPOTSET.U
+      addrSpecific := n
+
+      stateReg := wPot
     }
     is(wPot){
-      
+      io.ena       := true.B
+      io.wr        := true.B
+      addrOffset   := OSPOTENTIAL.U
+      addrSpecific := n
+
+      for(i <- 0 until EVALUNITS){
+        io.cntrSels(i).writeDataSel := 1.U
+      }
+
+      aNext := 0.U
+      n := n + 1.U
+      when(n === (TMNEURONS-1).U){
+        stateReg := idle
+      }.otherwise{
+        stateReg := rRefrac
+      }
     }
   }
 }
