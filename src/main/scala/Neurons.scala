05a91f21e9f7e204977a9d9884ea28ace65430d1
Thonner <anthon.riber@yahoo.com>
2020-04-14 15:59:29 +0200

    Neurons top module

--- src/main/scala/Neurons.scala
+++ src/main/scala/Neurons.scala
@@ -425,3 +425,48 @@ class ControlUnit extends Module{
     }
   }
 }
+
+
+class Neurons(coreID : Int) extends Module{
+  val io = IO(new Bundle{
+    //For axon system
+    val inOut      = Output(Bool())
+    val spikeCnt   = Input(UInt(AXONIDWIDTH.W))
+    val aAddr      = Output(UInt(AXONIDWIDTH.W))
+    val aEna       = Output(Bool())
+    val aData      = Input(UInt(AXONIDWIDTH.W))
+    //For spike transmission system
+    val n          = Output(UInt(N.W))
+    val spikes     = Output(Vec(EVALUNITS, Bool()))
+  })
+
+  val controlUnit = Module(new ControlUnit)
+  val evalUnits   = (0 until EVALUNITS).map(i => Module(new NeuronEvaluator))
+  val evalMems    = (0 until EVALUNITS).map(i => Module(new EvaluationMemory(coreID, i)))
+  
+  io.inOut                := controlUnit.io.inOut
+  controlUnit.io.spikeCnt := io.spikeCnt
+  io.aAddr                := controlUnit.io.aAddr
+  io.aEna                 := controlUnit.io.aEna
+  controlUnit.io.aData    := io.aData
+
+  io.n                    := controlUnit.io.n
+
+  for(i <- 0 until EVALUNITS){
+    io.spikes(i) := controlUnit.io.spikes(i)
+
+    evalUnits(i).io.dataIn                := evalMems(i).io.readData
+    evalMems(i).io.writeData              := evalUnits(i).io.dataOut
+    controlUnit.io.spikeIndi(i)           := evalUnits(i).io.spikeIndi
+    controlUnit.io.refracIndi(i)          := evalUnits(i).io.refracIndi
+    evalUnits(i).io.cntrSels.potSel       := controlUnit.io.cntrSels(i).potSel
+    evalUnits(i).io.cntrSels.spikeSel     := controlUnit.io.cntrSels(i).spikeSel
+    evalUnits(i).io.cntrSels.refracSel    := controlUnit.io.cntrSels(i).refracSel
+    evalUnits(i).io.cntrSels.writeDataSel := controlUnit.io.cntrSels(i).writeDataSel
+
+    evalMems(i).io.addr                   := controlUnit.io.addr
+    evalMems(i).io.wr                     := controlUnit.io.wr
+    evalMems(i).io.ena                    := controlUnit.io.ena
+  }
+
+}
