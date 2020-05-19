ab81c3bbf5065d2a1b442ec987bff2046acc6b6a
Thonner <anthon.riber@yahoo.com>
2020-05-19 12:06:15 +0200

    First draft off outputcore implemented not tested

--- src/main/scala/OutputCore.scala
+++ src/main/scala/OutputCore.scala
@@ -1,1 +1,54 @@
 import chisel3._
+import chisel3.util._
+import Constants._
+
+// The output core is for now hardcoded to deal with the specific showcase network
+class OutputCore(coreID : Int) extends Module{
+  val io = IO(new Bundle{
+    // to off chip communication
+    val offCCData  = Output(UInt(8.W))
+    val offCCValid = Output(Bool())
+    val offCCReady = Input(Bool())
+
+    //to bus 
+    val grant = Input(Bool())
+    val req   = Output(Bool())
+    val tx    = Output(UInt(GLOBALADDRWIDTH.W))
+    val rx    = Input(UInt(GLOBALADDRWIDTH.W))
+  })
+
+  //interface to/from bus
+  val interface  = Module(new BusInterface(coreID))
+  interface.io.grant      := io.grant
+  io.req                  := interface.io.reqOut
+  io.tx                   := interface.io.tx
+  interface.io.rx         := io.rx
+  interface.io.reqIn      := false.B //Never sends anything
+  interface.io.spikeID    := 0.U //Never sends anything
+
+  //deal with
+  val offCCData  = RegInit(0.U(8.W))
+  val offCCValid = RegInit(false.B)
+  
+  io.offCCValid := offCCValid
+  io.offCCData  := offCCData
+
+  when(offCCValid && io.offCCReady){
+    offCCValid := false.B
+  }
+
+  when (interface.io.valid){
+    offCCValid := true.B
+    when (interface.io.axonID(AXONIDWIDTH-1,AXONIDWIDTH-2) === 0.U){
+      offCCData := interface.io.axonID(7,0)
+    }.otherwise{
+      offCCData := interface.io.axonID(7,0) + 100.U //the first 100 output neurons are in core 2 the next 100 in core 3
+    }
+  }.elsewhen(offCCValid && io.offCCReady){
+    offCCValid := false.B
+  }
+}
+
+object OutputCore extends App {
+  chisel3.Driver.execute(Array("--target-dir", "build"), () => new OutputCore(6))
+}
