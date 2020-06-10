6c848238ed1927fae909be835f820e2d78dd4f92
Thonner <anthon.riber@yahoo.com>
2020-06-10 10:44:07 +0200

    Two bugs fixed input core
    1:  Cases where spike shouldnt be produced
    2: generating spike pulse to transmission

--- src/main/scala/InputCore.scala
+++ src/main/scala/InputCore.scala
@@ -46,7 +46,7 @@ class InputCore(coreID : Int) extends Module{
   val spikePulse   = RegInit(VecInit(Seq.fill(EVALUNITS)(false.B)))  // used to deliver spike pulses to transmission
   val idle :: firstr :: spikegen :: lastgen :: sWait :: Nil = Enum(5)
   val stateReg     = RegInit(idle)
-  val sPulseDecSig = WireDefault(EVALUNITS.U((EVALUNITS+1).W))
+  val sPulseDecSig = WireDefault(EVALUNITS.U((log2Up(EVALUNITS)+1).W))
 
   io.offCCHSout := phase
 
@@ -117,13 +117,17 @@ class InputCore(coreID : Int) extends Module{
 
   //fsm
   val modRes = Wire(UInt(RATEWIDTH.W))
+  when(ts === 0.U || cntrRateData === 0.U){
+    modRes := 1.U
+  }.otherwise{
   modRes := ts % cntrRateData //TODO what about modulus with 0
+  }
 
   switch(stateReg) {
     is(idle) {
       ts := 0.U
       pixcnt := 0.U
-      when(io.offCCHSin != phase && tsCycleCnt === 0.U) {
+      when(io.offCCHSin =/= phase && tsCycleCnt === 0.U) {
         stateReg := firstr
         phase := ~phase
       }
@@ -136,19 +140,19 @@ class InputCore(coreID : Int) extends Module{
     is(spikegen) {
       cntrEna := true.B
       pixcnt := pixcnt + 1.U
-      when(cntrRateData != 0.U && modRes === 0.U){
-        sPulseDecSig := 0.U ## pixcntLate(3,0)
+      when(modRes === 0.U){
+        sPulseDecSig := 0.U(1.W) ## pixcntLate(log2Up(EVALUNITS)-1,0)
       }
 
-      when(pixcnt === (INPUTSIZE-1).U){
+      when(pixcnt === (NEURONSPRCORE-1).U){
         stateReg := lastgen
       }
 
     }
     is(lastgen) {
       ts := ts + 1.U
-      when(cntrRateData != 0.U && modRes === 0.U){
-        sPulseDecSig := 0.U ## pixcntLate(3,0)
+      when(cntrRateData =/= 0.U && modRes === 0.U){
+        sPulseDecSig := 0.U(1.W) ## pixcntLate(log2Up(EVALUNITS)-1,0)
       }
 
       when(ts === 499.U){// all input steps done
@@ -167,13 +171,14 @@ class InputCore(coreID : Int) extends Module{
 
   // spike pulse decoder
   for (i <- 0 until EVALUNITS) {
+    spikePulse(i) := false.B
     when (sPulseDecSig === i.U) {
       spikePulse(i) := true.B
     }
   }
 
   // transmission last connections
-  spikeTrans.io.n         := pixcntLater(RATEADDRWIDTH-1, 4)
+  spikeTrans.io.n         := pixcntLater(RATEADDRWIDTH-1, RATEADDRWIDTH-N)
   for (i <- 0 until EVALUNITS) {
     spikeTrans.io.spikes(i) := spikePulse(i)
   }
