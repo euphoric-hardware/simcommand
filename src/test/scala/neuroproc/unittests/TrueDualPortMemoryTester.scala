7e260f148ea1a772e148db059fe31a4b3c3d3442
hansemandse <hansjakobdamsgaard@gmail.com>
2021-04-08 22:06:13 +0200

    fix tests

--- src/test/scala/neuroproc/unittests/TrueDualPortMemoryTester.scala
+++ src/test/scala/neuroproc/unittests/TrueDualPortMemoryTester.scala
@@ -81,106 +81,110 @@ class TrueDualPortMemoryTester extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
 
-  it should "work in both Chisel and Verilog" in {
-    Seq(true, false).foreach { 
-      synth =>
-      test(TrueDualPortMemory(addrW, dataW, synth))
-        .withAnnotations(Seq(VerilatorBackendAnnotation, WriteVcdAnnotation)) {
-        dut => 
-          dut.clock.setTimeout(10 * (1 << addrW))
-          val clka = dut.io.clka
-          val clkb = dut.io.clkb
+  def testFn(dut: TrueDualPortMemory) = {
+    dut.clock.setTimeout(10 * (1 << addrW))
+    val clka = dut.io.clka
+    val clkb = dut.io.clkb
 
-          def stepAndAdvance(clk: Bool) = {
-            clk.poke(true.B)
-            dut.clock.step()
-            clk.poke(false.B)
-          }
-        
-          // Model the memory as a mutable array
-          var ram = Array.fill(1 << addrW) { BigInt(0) }
-        
-          // Default values
-          dut.io.ena.poke(false.B)
-          dut.io.wea.poke(false.B)
-          dut.io.addra.poke(0.U)
-          dut.io.dia.poke(0.U)
-          clka.poke(false.B)
-        
-          dut.io.enb.poke(false.B)
-          dut.io.web.poke(false.B)
-          dut.io.addrb.poke(0.U)
-          dut.io.dib.poke(0.U)
-          clkb.poke(false.B)
-
-          // Check that the memory is all zeros
-          val portBc = fork {
-            dut.io.enb.poke(true.B)
-            for (i <- 0 until (1 << addrW)) {
-              dut.io.addrb.poke(i.U)
-              stepAndAdvance(clkb)
-              dut.io.dob.expect(0.U)
-            }
-            dut.io.enb.poke(false.B)
-          }
-
-          dut.io.ena.poke(true.B)
-          for (i <- 0 until (1 << addrW)) {
-            dut.io.addra.poke(i.U)
-            stepAndAdvance(clka)
-            dut.io.doa.expect(0.U)
-          }
-          dut.io.ena.poke(false.B)
-          portBc.join
-        
-          // Fork a handler for port b
-          val portBw = fork {
-            dut.io.enb.poke(true.B)
-            (writeAddrBs, writeDatas, weBs).zipped.toArray.foreach { x =>
-              dut.io.addrb.poke(x._1.U)
-              dut.io.dib.poke(x._2.U)
-              dut.io.web.poke(x._3.B)
-              if (x._3)
-                ram(x._1.toInt) = x._2
-              stepAndAdvance(clkb)
-            }
-            dut.io.enb.poke(false.B)
-          } 
-        
-          // Write on port a
-          dut.io.ena.poke(true.B)
-          (writeAddrAs, writeDatas, weAs).zipped.toArray.foreach { x =>
-            dut.io.addra.poke(x._1.U)
-            dut.io.dia.poke(x._2.U)
-            dut.io.wea.poke(x._3.B)
-            if (x._3)
-              ram(x._1.toInt) = x._2
-            stepAndAdvance(clka)
-          }
-          dut.io.ena.poke(false.B)
-          portBw.join()
-        
-          // Fork a handler for port b
-          val portBr = fork {
-            dut.io.enb.poke(true.B)
-            for (i <- 0 until (1 << addrW)) {
-              dut.io.addrb.poke(i.U)
-              stepAndAdvance(clkb)
-              dut.io.dob.expect(ram(i).U)
-            }
-            dut.io.enb.poke(false.B)
-          }
-        
-          // Read out values on port a
-          dut.io.ena.poke(true.B)
-          for (i <- 0 until (1 << addrW)) {
-            dut.io.addra.poke(i.U)
-            stepAndAdvance(clka)
-            dut.io.doa.expect(ram(i).U)
-          }
-          dut.io.ena.poke(false.B)
-          portBr.join
-      }
+    def stepAndAdvance(clk: Bool) = {
+      clk.poke(true.B)
+      dut.clock.step()
+      clk.poke(false.B)
     }
+      
+    // Model the memory as a mutable array
+    var ram = Array.fill(1 << addrW) { BigInt(0) }
+      
+    // Default values
+    dut.io.ena.poke(false.B)
+    dut.io.wea.poke(false.B)
+    dut.io.addra.poke(0.U)
+    dut.io.dia.poke(0.U)
+    clka.poke(false.B)
+  
+    dut.io.enb.poke(false.B)
+    dut.io.web.poke(false.B)
+    dut.io.addrb.poke(0.U)
+    dut.io.dib.poke(0.U)
+    clkb.poke(false.B)
+        
+    // Check that the memory is all zeros
+    val portBc = fork {
+      dut.io.enb.poke(true.B)
+      for (i <- 0 until (1 << addrW)) {
+        dut.io.addrb.poke(i.U)
+        stepAndAdvance(clkb)
+        dut.io.dob.expect(0.U)
+      }
+      dut.io.enb.poke(false.B)
+    }
+    dut.io.ena.poke(true.B)
+    for (i <- 0 until (1 << addrW)) {
+      dut.io.addra.poke(i.U)
+      stepAndAdvance(clka)
+      dut.io.doa.expect(0.U)
+    }
+    dut.io.ena.poke(false.B)
+    portBc.join
+      
+    // Fork a handler for port b
+    val portBw = fork {
+      dut.io.enb.poke(true.B)
+      (writeAddrBs, writeDatas, weBs).zipped.toArray.foreach { x =>
+        dut.io.addrb.poke(x._1.U)
+        dut.io.dib.poke(x._2.U)
+        dut.io.web.poke(x._3.B)
+        if (x._3)
+          ram(x._1.toInt) = x._2
+        stepAndAdvance(clkb)
+      }
+      dut.io.enb.poke(false.B)
+    } 
+      
+    // Write on port a
+    dut.io.ena.poke(true.B)
+    (writeAddrAs, writeDatas, weAs).zipped.toArray.foreach { x =>
+      dut.io.addra.poke(x._1.U)
+      dut.io.dia.poke(x._2.U)
+      dut.io.wea.poke(x._3.B)
+      if (x._3)
+        ram(x._1.toInt) = x._2
+      stepAndAdvance(clka)
+    }
+    dut.io.ena.poke(false.B)
+    portBw.join()
+      
+    // Fork a handler for port b
+    val portBr = fork {
+      dut.io.enb.poke(true.B)
+      for (i <- 0 until (1 << addrW)) {
+        dut.io.addrb.poke(i.U)
+        stepAndAdvance(clkb)
+        dut.io.dob.expect(ram(i).U)
+      }
+      dut.io.enb.poke(false.B)
+    }
+  
+    // Read out values on port a
+    dut.io.ena.poke(true.B)
+    for (i <- 0 until (1 << addrW)) {
+      dut.io.addra.poke(i.U)
+      stepAndAdvance(clka)
+      dut.io.doa.expect(ram(i).U)
+    }
+    dut.io.ena.poke(false.B)
+    portBr.join
+  }
+
+  it should "work in both Chisel and Verilog" in {
+    test(TrueDualPortMemory(addrW, dataW, true))
+      .withAnnotations(Seq(VerilatorBackendAnnotation, WriteVcdAnnotation)) {
+        dut => testFn(dut)
+      }
+    
+    test(TrueDualPortMemory(addrW, dataW, false))
+      .withAnnotations(Seq(VerilatorBackendAnnotation, WriteVcdAnnotation)) {
+        dut => testFn(dut)
+      }
   }
 }
