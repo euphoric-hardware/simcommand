f303975a61b53a85f2758f5e7982580aaaea95f3
Young-Jin Park <youngjinpark20@gmail.com>
2023-01-11 20:40:27 -0800

    add specification of repeatCollect

--- test/simcommand/LibrarySpec.scala
+++ test/simcommand/LibrarySpec.scala
@@ -9,9 +9,26 @@ import org.scalatest.flatspec.AnyFlatSpec
 class LibrarySpec extends AnyFlatSpec with ChiselScalatestTester {
   class LongDelay extends Module {
     val a = IO(Output(Bool()))
+    val b = IO(Output(UInt(32.W)))
     val c = Counter(2 << 16)
     c.inc()
     a := c.value > 100000.U
+    b := c.value
+  }
+
+  "repeatCollect" should "collect values" in {
+    test(new LongDelay()).withAnnotations(Seq(WriteVcdAnnotation, VerilatorBackendAnnotation, NoThreadingAnnotation)) { c =>
+      val program = repeatCollect(for {
+        r <- peek(c.b)
+        _ <- step(1)
+      } yield r.litValue, 10000)
+      c.clock.setTimeout(200000)
+
+      val result = unsafeRun(program, c.clock)
+      val expected = Seq.range(0, 10000)
+
+      assert(result.retval == expected)
+    }
   }
 
   "doWhile" should "not overflow the stack" in {
