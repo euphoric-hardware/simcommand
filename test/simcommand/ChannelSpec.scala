047f6ff1babeaea2a5891b24448e44a6c33ed897
Young-Jin Park <youngjinpark20@gmail.com>
2023-01-11 21:38:55 -0800

    implement nonempty and add spec

--- test/simcommand/ChannelSpec.scala
+++ test/simcommand/ChannelSpec.scala
@@ -6,38 +6,51 @@ import chisel3.util.Counter
 import org.scalatest.flatspec.AnyFlatSpec
 
 class ChannelSpec extends AnyFlatSpec with ChiselScalatestTester {
-  "getBlocking" should "inspect the circuit IO and get its value" in {
-    class Noop extends Module {}
+  class Timer extends Module {
+    val a = IO(Output(UInt(32.W)))
+    val c = Counter(16)
+    c.inc()
+    a := c.value
+  }
 
-    test(new Noop()) { c =>
-      def putter(ch: ChannelHandle[Integer]): Command[Unit] = for {
-        _ <- put[Integer](ch, 1)
-        _ <- put[Integer](ch, 2)
+  "getBlocking" should "inspect the circuit IO and get its value" in {
+    test(new Timer()) { c =>
+      def putter(ch: ChannelHandle[BigInt]): Command[Unit] = for {
+        _ <- put[BigInt](ch, 1)
+        _ <- put[BigInt](ch, 2)
         _ <- step(1)
-        _ <- put[Integer](ch, 3)
-        _ <- put[Integer](ch, 4)
+        _ <- put[BigInt](ch, 3)
+        _ <- put[BigInt](ch, 4)
         _ <- step(1)
-        _ <- put[Integer](ch, 5)
-        _ <- put[Integer](ch, 6)
+        _ <- put[BigInt](ch, 5)
+        _ <- put[BigInt](ch, 6)
         _ <- step(1)
-        _ <- put[Integer](ch, 7)
-        _ <- put[Integer](ch, 8)
+        _ <- put[BigInt](ch, 7)
+        _ <- put[BigInt](ch, 8)
         _ <- step(1)
       } yield ()
 
-      def getter(ch: ChannelHandle[Integer]): Command[Seq[Integer]] = for {
+      def getter(ch: ChannelHandle[BigInt]): Command[Seq[BigInt]] = for {
+        t0 <- peek(c.a)
         v1 <- getBlocking(ch)
+        t1 <- peek(c.a)
         v2 <- getBlocking(ch)
+        t2 <- peek(c.a)
         v3 <- getBlocking(ch)
+        t3 <- peek(c.a)
         v4 <- getBlocking(ch)
+        t4 <- peek(c.a)
         v5 <- getBlocking(ch)
+        t5 <- peek(c.a)
         v6 <- getBlocking(ch)
+        t6 <- peek(c.a)
         v7 <- getBlocking(ch)
+        t7 <- peek(c.a)
         v8 <- getBlocking(ch)
-      } yield (Seq(v1, v2, v3, v4, v5, v6, v7, v8))
+      } yield (Seq(t0.litValue, v1, t1.litValue, v2, t2.litValue, v3, t3.litValue, v4, t4.litValue, v5, t5.litValue, v6, t6.litValue, v7, t7.litValue, v8))
 
       val program = for {
-        ch <- makeChannel[Integer](1)
+        ch <- makeChannel[BigInt](1)
         // Spawn thread which will put two elements repeatedly into a channel
         t1 <- fork(putter(ch), "put")
         // Spawn thread which pops whenever an element is available
@@ -47,7 +60,24 @@ class ChannelSpec extends AnyFlatSpec with ChiselScalatestTester {
 
       val result = unsafeRun(program, c.clock)
 
-      assert(result.retval == Seq(1, 2, 3, 4, 5, 6, 7, 8))
+      assert(result.retval == Seq(0, 1, 0, 2, 0, 3, 1, 4, 1, 5, 2, 6, 2, 7, 3, 8))
+    }
+  }
+
+  "nonEmpty" should "test whether or not a channel is empty" in {
+    test(new Timer()) { c =>
+      val program = for {
+        ch <- makeChannel[BigInt](1)
+        v1 <- nonEmpty(ch)
+        _ <- put[BigInt](ch, 2)
+        v2 <- nonEmpty(ch)
+        _ <- getBlocking(ch)
+        v3 <- nonEmpty(ch)
+      } yield (v1, v2, v3)
+
+      val result = unsafeRun(program, c.clock)
+
+      assert(result.retval == (false, true, false))
     }
   }
 }
